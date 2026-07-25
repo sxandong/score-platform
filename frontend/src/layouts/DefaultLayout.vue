72c20af6f2e22097a9bb99e55a278cb11fb6503c
Developer <dev@score-platform.local>
2026-07-26 07:55:48 +0800

    swap score query and cutoffs in sidebar order

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -26,8 +26,8 @@
           <template #title><el-icon><Edit /></el-icon><span>成绩管理</span></template>
           <el-menu-item v-if="auth.hasPermission('score:create')" index="/scores/entry">成绩录入</el-menu-item>
           <el-menu-item v-if="auth.hasPermission('score:create')" index="/scores/import">批量导入</el-menu-item>
-          <el-menu-item v-if="auth.hasPermission('score:read')" index="/scores/query">成绩查询</el-menu-item>
           <el-menu-item index="/scores/cutoffs">分数线设置</el-menu-item>
+          <el-menu-item v-if="auth.hasPermission('score:read')" index="/scores/query">成绩查询</el-menu-item>
         </el-sub-menu>
         <el-sub-menu v-if="auth.hasPermission('analysis:view')" index="analysis">
           <template #title><el-icon><TrendCharts /></el-icon><span>统计分析</span></template>
